b9be75bf90a5633a66bcd2edc380d2092f374078
AV2409 <142912539+AV2409@users.noreply.github.com>
2024-07-20 13:53:22 +0530

    Time: 115 ms (6.23%) | Memory: 49.3 MB (6.87%) - LeetSync

--- 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.cpp
+++ 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.cpp
@@ -6,7 +6,7 @@ public:
 
         // map<int,int> mp;
 
-        unordered_set<int> s;
+        set<int> s;
 
         for(int i=0;i<nums.size();i++){
             if(s.count(nums[i])==1) ans.push_back(nums[i]);
