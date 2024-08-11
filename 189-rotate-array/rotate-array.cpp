bd42a2d2223894df821344778f55ec01def02b71
AV2409 <142912539+AV2409@users.noreply.github.com>
2024-08-11 18:30:29 +0530

    Time: 19 ms (69.58%) | Memory: 28.9 MB (5.33%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -5,8 +5,7 @@ public:
 
     vector<int> temp;
     int x=nums.size()-k;
-    if(nums.size()==1) {temp.push_back(nums[0]);
-    return;}
+    if(nums.size()==1) return;
 
     for(int i=x;i<nums.size();i++){
         temp.push_back(nums[i]);
